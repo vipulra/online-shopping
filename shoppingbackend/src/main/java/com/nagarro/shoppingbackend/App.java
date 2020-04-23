18501fca812465f4157ea25027091b82f168aa7b
vipul <vipulra.7@gmail.com>
2020-04-23 19:39:10 +0530

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/com/nagarro/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.nagarro.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
